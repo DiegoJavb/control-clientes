2ae7170b1ebcfaba95bd317a35828a8ac1ff0f9e
Diego Bacuy <diegohd-20@outlook.com>
2023-04-13 13:02:16 -0500

    validacion para campo nombre

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ClienteServicio } from 'src/app/servicios/cliente.service';
 import { Cliente } from 'src/app/modelo/cliente.model';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { NgClass } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
@@ -9,7 +12,16 @@ import { Cliente } from 'src/app/modelo/cliente.model';
 })
 export class ClientesComponent implements OnInit {
   clientes?: Cliente[];
-  constructor(private clientesServicio: ClienteServicio) {}
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    saldo: 0,
+    email: '',
+  };
+  constructor(
+    private clientesServicio: ClienteServicio,
+    private flasMessagesService: FlashMessagesService
+  ) {}
   ngOnInit(): void {
     this.clientesServicio.getClientes().subscribe((clientes) => {
       this.clientes = clientes;
@@ -23,5 +35,18 @@ export class ClientesComponent implements OnInit {
       });
     }
     return saldoTotal;
+  }
+  agregar({ value, valid }: NgForm) {
+    if (!valid) {
+      this.flasMessagesService.show(
+        'Por favor llenar formulario correctamente',
+        {
+          cssClass: 'alert-danger',
+          timeout: 4000,
+        }
+      );
+    } else {
+      // Agregar el nuevo cliente
+    }
   }
 }
